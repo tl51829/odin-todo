e7dea7f0322b9842385b01200fe4f75b7323d98c
test <test@gmail.com>
2025-01-10 14:39:55 +0800

    Add base HTML code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import "./css/reset.css";
 import "./css/styles.css";
 
-import { greeting } from "./greeting.js";
+// import { greeting } from "./greeting.js";
 
-console.log(greeting);
+// console.log(greeting);
